b5c2c48cb27103ef0a3a80268e22909201018157
Jelena Jovanovic <djurovic.jelena@gmail.com>
2018-02-09 16:42:51 +0100

    Moj data

---
+++ data.py
@@ -0,0 +1,60 @@
+def Projects():
+    projects = [
+            {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+            },
+        {
+            'id':1,
+            'title':'The Jungle Book',
+            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': 'https://hdwallsource.com/img/2016/9/the-jungle-book-movie-desktop-wallpaper-51835-53541-hd-wallpapers.jpg',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': 'https://images5.alphacoders.com/789/789272.jpg',
+            'img_3': 'http://www.maacindia.com/blog/wp-content/uploads/2016/04/sketches-of-characters-from-disney.jpg',
+            'img_4': ''
+         },
+        {
+            'id':2,
+            'title':'The Martian',
+            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': 'https://nerdist.com/wp-content/uploads/2015/10/MartianDiff_FEAT.jpg',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': '',
+            'img_3': '',
+            'img_4': ''
+         },
+        {
+            'id':3,
+            'title':'Exodus',
+            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': 'http://dotb.tc0bblfg2d81v7kurec.netdna-cdn.com/wp-content/uploads/2015/12/Exodus-Gods-and-Kings.jpg',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': '',
+            'img_3': '',
+            'img_4': ''
+         },
+        {
+            'id':4,
+            'title':'Gardians of the galaxy',
+            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': ' ',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': '',
+            'img_3': '',
+            'img_4': ''
+         }
+    ]
+
+    return projects
