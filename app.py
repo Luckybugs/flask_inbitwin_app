bb139a4a0049656e0a0e6197243fc8b1090e412d
Jelena Jovanovic <djurovic.jelena#gmail.com>
2018-02-17 06:51:36 +0000

    Prckala u Londonu

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 from data import Projects
 
 app = Flask(__name__)
 
+
+# app.config.update(dict(
+#     MAIL_SERVER = 'smtp.googlemail.com',
+#     MAIL_PORT = 465,
+#     MAIL_USE_TLS = False,
+#     MAIL_USE_SSL = True,
+#     MAIL_USERNAME = 'djurovic.jelena@gmail.com',
+#     MAIL_PASSWORD = 'DjoleDontPanic'
+# ))
+
+mail = Mail(app)
+
+
 Projects = Projects()
+
+klasa = 'grey darken-3 z-depth-0'
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', klasa = 'transparent darken-3 z-depth-0')
 
 @app.route('/showreel')
 def showreel():
-    return render_template('showreel.html')
+    return render_template('showreel.html', klasa = klasa)
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html', projects = Projects)
+    return render_template('projects.html', projects = Projects, klasa = klasa)
 
-@app.route('/about')
+@app.route('/projects/<string:title>/')
+def project(title):
+    for p in Projects:
+        if title in p.values():
+            current = p
+    return render_template('project.html', title = title, current = current, klasa = klasa)
+
+@app.route('/about' )
 def about():
-    return render_template('about.html')
+
+    # msg = Message('Test', sender='djurovic.jelena@gmail.com', recipients=['djurovic.jelena@gmail.com'])
+    # msg.body = 'This is a test email' #Customize based on user input
+    # mail.send(msg)
+    return render_template('about.html', klasa = klasa)
 
 
 @app.route('/article')
--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 def Projects():
     projects = [
 
-            {
+        {
             'id':1,
             'title':'The Jungle Book',
-            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'img_1': 'https://hdwallsource.com/img/2016/9/the-jungle-book-movie-desktop-wallpaper-51835-53541-hd-wallpapers.jpg',
             'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
@@ -12,11 +12,11 @@ def Projects():
             'img_2': 'https://images5.alphacoders.com/789/789272.jpg',
             'img_3': 'http://www.maacindia.com/blog/wp-content/uploads/2016/04/sketches-of-characters-from-disney.jpg',
             'img_4': ''
-            },
-            {
+        },
+        {
             'id':2,
             'title':'The Martian',
-            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'img_1': 'https://nerdist.com/wp-content/uploads/2015/10/MartianDiff_FEAT.jpg',
             'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
@@ -24,11 +24,11 @@ def Projects():
             'img_2': '',
             'img_3': '',
             'img_4': ''
-            },
-            {
+        },
+        {
             'id':3,
             'title':'Exodus',
-            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'img_1': 'http://dotb.tc0bblfg2d81v7kurec.netdna-cdn.com/wp-content/uploads/2015/12/Exodus-Gods-and-Kings.jpg',
             'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
@@ -36,11 +36,11 @@ def Projects():
             'img_2': '',
             'img_3': '',
             'img_4': ''
-            },
-            {
+        },
+        {
             'id':4,
             'title':'Gardians of the galaxy',
-            'Head text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'img_1': ' ',
             'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
             'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
@@ -48,7 +48,31 @@ def Projects():
             'img_2': '',
             'img_3': '',
             'img_4': ''
-            }
+        },
+        {
+            'id':5,
+            'title':'Fantastic Beasts',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': ' ',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': '',
+            'img_3': '',
+            'img_4': ''
+        },
+        {
+            'id':5,
+            'title':'Star Wars: The Last Jedi',
+            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'img_1': ' ',
+            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
+            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
+            'video_1_text': '',
+            'img_2': '',
+            'img_3': '',
+            'img_4': ''
+        }
     ]
 
     return projects
