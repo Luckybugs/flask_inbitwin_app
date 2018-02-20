839de05dbbddf64d3c7e511453a70ad6c04c44f8
Jelena Jovanovic <djurovic.jelena#gmail.com>
2018-02-20 11:15:27 +0000

    dodao mail formu da radi kao

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_mail import Mail, Message
+from wtforms import Form, StringField, TextAreaField, validators
 
 from data import Projects
 
 app = Flask(__name__)
 
 
-# app.config.update(dict(
-#     MAIL_SERVER = 'smtp.googlemail.com',
-#     MAIL_PORT = 465,
-#     MAIL_USE_TLS = False,
-#     MAIL_USE_SSL = True,
-#     MAIL_USERNAME = 'djurovic.jelena@gmail.com',
-#     MAIL_PASSWORD = 'DjoleDontPanic'
-# ))
+app.config.update(dict(
+    MAIL_SERVER = 'smtp.googlemail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'djurovic.jelena@gmail.com',
+    MAIL_PASSWORD = 'DjoleDontPanic'
+))
 
 mail = Mail(app)
 
@@ -41,13 +42,7 @@ def project(title):
             current = p
     return render_template('project.html', title = title, current = current)
 
-@app.route('/about' )
-def about():
 
-    # msg = Message('Test', sender='djurovic.jelena@gmail.com', recipients=['djurovic.jelena@gmail.com'])
-    # msg.body = 'This is a test email' #Customize based on user input
-    # mail.send(msg)
-    return render_template('about.html')
 
 
 @app.route('/article')
@@ -55,8 +50,36 @@ def article():
     return render_template('article.html')
 
 
+class EmailForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('email', [
+        validators.DataRequired(),
+        validators.Length(min=6, max=50)
+        ])
+    message = TextAreaField('message', [validators.Length(min=10)])
+
+@app.route('/about', methods=["GET", "POST"] )
+def about():
+    form = EmailForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        flash('jebote', 'success')
+        msg = Message('Test', sender=email, recipients=['djurovic.jelena@gmail.com'])
+        msg.body = message #Customize based on user input
+        mail.send(msg)
+
+        return redirect(url_for('about'))
+
+
+    return render_template('about.html', form=form)
+
 
 
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
     
