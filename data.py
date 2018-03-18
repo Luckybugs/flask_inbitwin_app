f3ecf590712826afd7238f92224a01bc7bd974c8
Jelena Jovanovic <djurovic.jelena@gmail.com>
2018-03-18 15:32:38 +0000

    spremam se za push online

--- data.py
+++ data.py
@@ -41,9 +41,9 @@ def Projects():
             'media':
                     {
                         'img_01': 'images/jungle_book/001.jpg',
-                        'img_01_text': 'tekst uz sliku 1',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
                         'img_02': 'images/jungle_book/002.jpg',
-                        'img_02_text': 'tekst uz sliku 2',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
                         'img_03': 'images/jungle_book/003.jpg',
                         'img_03_text': 'tekst uz sliku 3',
                         'img_04': 'images/jungle_book/004.jpg',
@@ -85,17 +85,43 @@ def Projects():
             'head_text':'Lorem ipsum dolor sit amet!',
             'media':
                     {
-                        'img_1': '',
-                        'img_1_text':'Tekst uz sliku 1',
-                        'video_1':'',
-                        'video_1_text': '',
-                        'img_2': 'https://images5.alphacoders.com/789/789272.jpg',
-                        'img_2_text':'tekst uz sliku 2',
-                        'img_3': 'http://www.maacindia.com/blog/wp-content/uploads/2016/04/sketches-of-characters-from-disney.jpg',
-                        'img_3_text':'tekst uz sliku 3',
-                        'img_4': '',
-                        'img_4_text':'',
-                        
+                        'img_01': 'images/passengers/001.jpg',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
+                        'img_02': 'images/passengers/002.jpg',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
+                        'img_03': 'images/passengers/003.jpg',
+                        'img_03_text': 'tekst uz sliku 3',
+                        'img_04': 'images/passengers/004.jpg',
+                        'img_04_text': '',
+                        'img_05': 'images/passengers/005.jpg',
+                        'img_05_text': '',
+                        'img_06': 'images/passengers/006.jpg',
+                        'img_06_text': '',
+                        'img_07': 'images/passengers/007.jpg',
+                        'img_07_text': '',
+                        'img_08': 'images/passengers/008.jpg',
+                        'img_08_text': '',
+                        'img_09': 'images/passengers/009.jpg',
+                        'img_09_text': '',
+                        'img_10': 'images/passengers/010.jpg',
+                        'img_10_text': '',
+ 
+                        'img_12': 'images/passengers/012.jpg',
+                        'img_12_text': '',
+                        'img_13': 'images/passengers/013.jpg',
+                        'img_13_text': '',
+                        'img_14': 'images/passengers/014.jpg',
+                        'img_14_text': '',
+                        'img_15': 'images/passengers/015.jpg',
+                        'img_15_text': '',
+                        'img_16': 'images/passengers/016.jpg',
+                        'img_16_text': '',
+                        'img_17': 'images/passengers/016.jpg',
+                        'img_17_text': '',
+                        'img_18': 'images/passengers/016.jpg',
+                        'img_18_text': '',
+  
+
                    }
         },
 
@@ -106,13 +132,21 @@ def Projects():
             'year': '2015',
             'hero_image': 'images/Martian.jpg',
             'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'img_1': 'images/Martian.jpg',
-            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
-            'video_1_text': '',
-            'img_2': '',
-            'img_3': '',
-            'img_4': ''
+            'media':
+                    {
+                        'img_01': 'images/martian/001.jpg',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
+                        'img_02': 'images/martian/002.jpg',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
+                        'img_03': 'images/martian/003.png',
+                        'img_03_text': 'tekst uz sliku 3',
+                        'img_04': 'images/martian/004.png',
+                        'img_04_text': '',
+                        'img_05': 'images/martian/005.jpg',
+                        'img_05_text': '',
+                        'img_06': 'images/martian/006.jpg',
+                        'img_06_text': '',
+                    }
         },
         {
             'id':3,
@@ -121,27 +155,52 @@ def Projects():
             'year': '2014',
             'hero_image': 'images/Exodus.jpg',
             'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'img_1': 'images/Exodus.jpg',
-            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
-            'video_1_text': '',
-            'img_2': '',
-            'img_3': '',
-            'img_4': ''
+            'media':
+                    {
+                        'img_01': 'images/exodus/001.jpg',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
+                        'img_02': 'images/exodus/002.jpg',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
+                        'img_03': 'images/exodus/003.jpg',
+                        'img_03_text': 'tekst uz sliku 3',
+                        'img_04': 'images/exodus/004.jpg',
+                        'img_04_text': '',
+                        'img_05': 'images/exodus/005.jpg',
+                        'img_05_text': '',
+                        'img_07': 'images/exodus/007.jpg',
+                        'img_07_text': '',
+                        'img_08': 'images/exodus/008.jpg',
+                        'img_08_text': '',
+                        'img_09': 'images/exodus/009.jpg',
+                        'img_09_text': ''
+                    }
         },
         {
             'id':4,
             'title':'Gardians of the galaxy',
             'year': '2014',
             'hero_image': 'images/galaxy.jpg',
-            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'img_1': 'images/galaxy.jpg',
-            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
-            'video_1_text': '',
-            'img_2': '',
-            'img_3': '',
-            'img_4': ''
+            'head_text':'',
+            'media':
+                    {
+                        'img_01': 'images/guardians/001.jpg',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
+                        'img_02': 'images/guardians/002.png',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
+                        'img_03': 'images/guardians/003.jpg',
+                        'img_03_text': 'tekst uz sliku 3',
+                        'img_04': 'images/guardians/004.jpg',
+                        'img_04_text': '',
+                        'img_05': 'images/guardians/005.jpg',
+                        'img_05_text': '',
+                        'img_06': 'images/guardians/006.jpg',
+                        'img_06_text': '',
+                        'img_07': 'images/guardians/007.jpg',
+                        'img_07_text': '',
+                        'img_08': 'images/guardians/008.jpg',
+                        'img_08_text': '',
+                        
+                    }
         },
         {
             'id':5,
@@ -164,14 +223,27 @@ def Projects():
             'title':'Doctor Strange',
             'year': '2016',
             'hero_image': 'images/strange.jpg',
-            'head_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'img_1': 'images/strange.jpg',
-            'img_1_text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus id vel omnis, veniam nesciunt adipisci quis voluptatum, ex error laudantium, voluptate aspernatur, facere doloribus nisi placeat recusandae provident commodi quas!',
-            'video_1':'https://player.vimeo.com/video/170367649" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen',
-            'video_1_text': '',
-            'img_2': '',
-            'img_3': '',
-            'img_4': ''
+            'head_text':'',
+            'media':
+                    {
+                        'img_01': 'images/dr_strange/001.jpg',
+                        'img_01_text': 'Return a string which is the concatenation of the strings in the sequence. The separator between elements is an empty string per default, you can define it with the optional parameter',
+                        'img_02': 'images/dr_strange/002.jpg',
+                        'img_02_text': 'You can easily vertically center things by adding the class valign-wrapper to the container holding the items you want to vertically align',
+                        'img_03': 'images/dr_strange/003.jpg',
+                        'img_03_text': 'tekst uz sliku 3',
+                        'img_04': 'images/dr_strange/004.jpg',
+                        'img_04_text': '',
+                        'img_05': 'images/dr_strange/005.jpg',
+                        'img_05_text': '',
+                        'img_06': 'images/dr_strange/006.jpg',
+                        'img_06_text': '',
+                        'img_07': 'images/dr_strange/007.jpg',
+                        'img_07_text': '',
+                        'img_08': 'images/dr_strange/DoctorStrange_TimeWarp_ILM_VFX.jpg',
+                        'img_08_text': '',
+                        
+                    }
         }
 
 
